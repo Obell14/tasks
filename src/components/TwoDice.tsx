1154f24b6bec4bff262171ec95a6758eed73436d
Obell14 <owenbellew@gmail.com>
2022-10-23 21:32:04 -0400

    Completed TwoDice

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,52 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [dice1, setDice1] = useState<number>(1);
+    const [dice2, setDice2] = useState<number>(2);
+
+    return (
+        <div>
+            <span data-testid="left-die">{dice1}</span>
+            <span data-testid="right-die">{dice2}</span>
+            <Button
+                onClick={() => {
+                    setDice1(d6());
+                }}
+            >
+                Roll left
+            </Button>
+            <Button
+                onClick={() => {
+                    setDice2(d6());
+                }}
+            >
+                Roll right
+            </Button>
+            {dice1 == 1 && dice2 == 1 ? (
+                <div>lose</div>
+            ) : dice1 == dice2 ? (
+                <div>win</div>
+            ) : (
+                <div></div>
+            )}
+        </div>
+    );
 }
+
+/*The TwoDice component will simulate a game where you roll two dice in an attempt to get matching values. 
+However, you lose the game if your dice ever come up as a pair of ones (“snake eyes”).
+
+-You will need two states, one for each die.
+-Each die’s value should be rendered in the View in a span tag of their 
+own, with the first dice having the data-testid of left-die and the second 
+dice having the data-testid of right-die.
+
+-You will need two “Roll” buttons (labelled Roll Left and Roll Right).
+-Clicking a Roll button will change the value for the corresponding dice 
+using the provided d6 function.
+
+-The initial values of the dice cannot be the same.
+-When the two states are 1, render a message that includes the word Lose.
+-When the two states are equal, render a message that includes the word Win.
+-If you do all these and are still not passing all tests, read the test file, 
+it is a good practice! In much of software engineering, the tests are the product spec.*/
