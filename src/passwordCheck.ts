4ef05c64c67a28f54a856c4723b5d7681cf2f72d
Obell14 <owenbellew@gmail.com>
2022-10-23 23:18:06 -0400

    temp Passwordcheck

---
+++ src/passwordCheck.ts
@@ -0,0 +1,20 @@
+export function dogpass(password: string): boolean {
+    return false;
+}
+
+/*using your answer from part (a), write a method that checks 
+the validity of a password string, and then write all of the 
+test cases for all the equivalence partitioning classes you 
+identified, using Jest (i.e. the same testing library that 
+Frontend-Fun uses). You should have at least one test 
+case per equivalence partition, both valid and invalid 
+partitions. You can use any external libraries that you 
+want for the implementation, and look at any online 
+examples of tests in Jest to help you write your test cases; 
+just include links to any of them in your answer below as comments. 
+
+Do this by creating a new branch from your task repository called 
+“dogcare_testing”. Push it to your Github tasks repo when complete, 
+and paste a github link to your final commit and a screenshot showing 
+your tests passing, and your implementation and test code below.
+ */
