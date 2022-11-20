2b4aeb8e86a7d87ac55791843ee4ce5fcf2c5d46
Obell14 <owenbellew@gmail.com>
2022-11-20 17:18:00 -0500

    Completed MultipleChoiceQuestion

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,26 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [current, setCurrent] = useState<string>(options[0]);
+
+    function updateCurrent(event: React.ChangeEvent<HTMLSelectElement>) {
+        setCurrent(event.target.value);
+    }
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="choice">
+                <Form.Label>Are you human?</Form.Label>
+                <Form.Select value={current} onChange={updateCurrent}>
+                    {options.map((temp: string) => (
+                        <option key={temp} value={temp}>
+                            {temp}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            {current === expectedAnswer ? "✔️" : "❌"}
         </div>
     );
 }
